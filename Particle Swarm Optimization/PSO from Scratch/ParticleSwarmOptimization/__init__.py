e40bfd0123fdfd813bff05c8bb41f26d7c7097b9
Gongas <goncalo.mesteves03@gmail.com>
2024-09-16 15:23:44 +0100

    Fixed some typos on the package imports

--- Particle Swarm Optimization/PSO from Scratch/ParticleSwarmOptimization/__init__.py
+++ Particle Swarm Optimization/PSO from Scratch/ParticleSwarmOptimization/__init__.py
@@ -1,10 +1,10 @@
 # This Python Package contains the code used in the developed Particle Swarm Optimization Algorithm
 
 # Defining which submodules to import when using from <package> import *
-__all__ = ["ResNet",
-           "MCTS", "AlphaMCTS",
-           "AlphaZero", "AlphaZeroParallel"]
+__all__ = ["Particle", "Optimization",
+           "loadConfig",
+           "sphereFunction", "rosenbrockFunction"]
 
 from .ParticleSwarmOptimization import (Particle, Optimization)
 from .Configuration import (loadConfig)
-from .Functions import (sphereFunction)
+from .Functions import (sphereFunction, rosenbrockFunction)
