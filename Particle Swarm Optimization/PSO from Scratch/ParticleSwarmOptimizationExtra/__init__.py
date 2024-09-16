834cf4936d5fc0a3c0465d6fc1991fb145a2328d
Gongas <goncalo.mesteves03@gmail.com>
2024-09-16 21:50:32 +0100

    Renamed the python package

--- Particle Swarm Optimization/PSO from Scratch/ParticleSwarmOptimizationExtra/__init__.py
+++ Particle Swarm Optimization/PSO from Scratch/ParticleSwarmOptimizationExtra/__init__.py
@@ -1,5 +1,7 @@
 # This Python Package contains the code used in the developed Particle Swarm Optimization Algorithm
 
+# NOTE: This package contains a different implementation of the Particle Swarm Optimization Algorithm than the one provided in the Notebook
+
 # Defining which submodules to import when using from <package> import *
 __all__ = ["Particle", "Optimization",
            "loadConfig",
