22cf0f3dfd52b5d6a5ede61a6487ae899c62acde
Gongas <goncalo.mesteves03@gmail.com>
2024-09-16 14:50:53 +0100

    Added the Rosenbrock function

--- Particle Swarm Optimization/PSO from Scratch/ParticleSwarmOptimization/Functions.py
+++ Particle Swarm Optimization/PSO from Scratch/ParticleSwarmOptimization/Functions.py
@@ -10,3 +10,16 @@ def sphereFunction(x:np.ndarray) -> float:
     := return: Approximation of the sphere function given a input array
     """
     return np.sum(np.square(x))
+
+def rosenbrockFunction(x:np.ndarray) -> float:
+    """
+    # Description:
+        -> Rosenbrock Function used inside the Research Paper [Summation of (x_i)^2, for i in [1, dim(x)]]
+    
+    := param x: Numpy array with an nth-dimensional shape
+    := return: Approximation of the function given a input array
+    """
+    total = 0.0
+    for idx in range(x.size - 1):
+        total += (100 * (x[idx]**2 - x[idx+  1])**2 + (x[idx] - 1)**2)
+    return total
